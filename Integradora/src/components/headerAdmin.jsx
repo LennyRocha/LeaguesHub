291b4c4acff6c30ba991c7886c393f9225cc8276
LennyRocha <20233tn094@utez.edu.mx>
2025-04-05 22:06:46 -0600

    Botón arreglado en sidebar del admin.

--- Integradora/src/components/headerAdmin.jsx
+++ Integradora/src/components/headerAdmin.jsx
@@ -387,7 +387,7 @@ function AdminDashboard() {
 
           <div className="text-center d-none d-md-inline">
             <button
-              className="rounded-circle border-0"
+              className="rounded-circle border-0 p-0"
               id="sidebarToggle"
             ></button>
           </div>
