904d674565393b67f085298234a999cf46acc9e3
LennyRocha <20233tn094@utez.edu.mx>
2025-04-08 23:04:30 -0600

    Mensajes de error personalizados.

--- Integradora/src/components/componentesExternos/NoAuthPage.jsx
+++ Integradora/src/components/componentesExternos/NoAuthPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "../../css/loading.css";
-import Balon from "../../assets/templates/balon.png";
-
 import lottie from "lottie-web";
 import { defineElement } from "@lordicon/element";
 // define "lord-icon" custom element with default properties
@@ -9,20 +7,29 @@ defineElement(lottie.loadAnimation);
 
 export default function NoAuthPage() {
   return (
-    <div className="centered">
+    <div className="err-container">
       <script src="https://cdn.lordicon.com/lordicon.js"></script>
-      <h1>Alto</h1>
-      <p className="body-small">No tienes permisos de acceso a esta pagina</p>
       <lord-icon
         src="https://cdn.lordicon.com/fttvwdlw.json"
         trigger="loop"
         stroke="bold"
-        state="loop-roll"
+        state="loop-flying"
         colors="primary:#333333,secondary:#9a0000"
-        style={{ width: "10rem", height: "10rem", MarginBottom: 20 }}
+        style={{ width: "20rem", height: "20rem", }}
       ></lord-icon>
-      <a href="https://lordicon.com/">Icons by Lordicon.com</a>
-      <a href="/">Volver</a>
+      <h2 className="text-center">¡Acceso denegado!</h2>
+      <p className="mx-0 px-0 text-center">No tienes permisos de acceso a esta pagina</p>
+      <div className="gap-2 w-100 justify-content-center d-flex mt-0">
+        <a href="/" className="link">Ir al menú principal</a>
+      </div>
+      {/* <lord-icon
+        src="/icons/detengase.json"
+        trigger="morph"
+        stroke="bold"
+        state="morph-stop"
+        colors="primary:#333333,secondary:#9a0000"
+        style={{ width: "3rem", height: "3rem", MarginBottom: 20 }}
+      ></lord-icon> */}
     </div>
   );
 }
